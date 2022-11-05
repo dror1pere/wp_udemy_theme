83c2ad0125094b30bf4c517d556caa7a8e7b7855
Dror Pere <dror1pere@gmail.com>
2022-11-05 14:06:21 +0200

    initial

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html <? language_attributes(); ?>>
+<head>
+    <meta charset=<?bloginfo('charset'); ?>>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <? wp_head();?>
+</head>
+<body <?body_class();?>>
+<?wp_body_open();?>
+    <h1>Hello World</>
+    <?wp_footer();?>
+</body>
+</html>
